72c4c534b4200e9ce160475294d243274313d130
BERNARD <guillaume-u.bernard@thalesgroup.com>
2021-07-07 12:04:26 +0200

    adding github project url

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(name='pygger',
 version='0.1',
 description='Swagger doc generator for python code',
-url='#',
+url='https://github.com/GBernard314/pygger',
 author='GBernard314',
 author_email='guillaume.bernard31415@gmail.com',
 license='MIT',
